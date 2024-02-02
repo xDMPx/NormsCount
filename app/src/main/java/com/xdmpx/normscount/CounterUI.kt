edd5d66b2c0c59385dc7dd24ef920cd928e2c62e
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-02-02 12:40:52 +0100

    Implement basic navigation

--- app/src/main/java/com/xdmpx/normscount/CounterUI.kt
+++ app/src/main/java/com/xdmpx/normscount/CounterUI.kt
@@ -11,26 +11,36 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class Counter {
     val TAG_DEBUG = "Counter"
-    private var count: MutableState<Int> = mutableStateOf(0)
+    private var count: MutableState<Int> = mutableIntStateOf(0)
 
     @Composable
     @Preview
@@ -87,4 +97,38 @@ class Counter {
             }
         }
     }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun CounterTopAppBar(onNavigateToSettings: () -> Unit) {
+        CenterAlignedTopAppBar(colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ), title = {
+            Text(
+                "NormsCount", maxLines = 1, overflow = TextOverflow.Ellipsis
+            )
+        }, actions = {
+            TopAppBarMenu(onNavigateToSettings)
+        })
+    }
+
+    @Composable
+    fun TopAppBarMenu(onNavigateToSettings: () -> Unit) {
+        var expanded by remember { mutableStateOf(false) }
+
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = Icons.Filled.Menu, contentDescription = "Top Bar Menu"
+            )
+        }
+
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            DropdownMenuItem(text = { Text(text = "Settings") }, onClick = {
+                expanded = false
+                onNavigateToSettings()
+            })
+        }
+    }
+
 }
--- app/src/main/java/com/xdmpx/normscount/MainActivity.kt
+++ app/src/main/java/com/xdmpx/normscount/MainActivity.kt
@@ -3,17 +3,22 @@ package com.xdmpx.normscount
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.xdmpx.normscount.ui.theme.NormsCountTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val counter = Counter()
 
         setContent {
             NormsCountTheme {
@@ -21,9 +26,36 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    counter.CounterUI()
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "main") {
+                        composable("main") { MainUI() { navController.navigate("settings") } }
+                        composable("settings") { Settings() { navController.navigate("main") } }
+                    }
                 }
             }
         }
     }
+
+    @Composable
+    fun MainUI(onNavigateToSettings: () -> Unit) {
+        val counter = Counter()
+
+        Scaffold(
+            topBar = { counter.CounterTopAppBar(onNavigateToSettings) },
+        ) { innerPadding ->
+            Box(Modifier.padding(innerPadding)) {
+                counter.CounterUI()
+            }
+        }
+    }
+
+    @Composable
+    fun Settings(onNavigateToMain: () -> Unit) {
+        Scaffold(
+            topBar = { },
+        ) { innerPadding ->
+            Box(Modifier.padding(innerPadding)) {}
+        }
+    }
+
 }
