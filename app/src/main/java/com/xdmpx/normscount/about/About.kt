92c47a47209096a954b3eed6bb4816a8135d2c60
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-03-16 16:16:27 +0100

    Enhance about screen appearance by replacing `Column` with `OutlinedCard`

--- app/src/main/java/com/xdmpx/normscount/about/About.kt
+++ app/src/main/java/com/xdmpx/normscount/about/About.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -16,10 +17,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -27,6 +32,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -54,17 +61,19 @@ object About {
                     .padding(innerPadding)
                     .fillMaxSize()
             ) {
-                Column {
+                OutlinedCard(
+                    border = BorderStroke(0.25.dp, Color.Gray), modifier = Modifier.padding(12.dp)
+                ) {
                     AppInfo()
                     AboutButton(text = "${stringResource(R.string.about_version)} v${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})") {}
                     AboutButton(text = stringResource(R.string.about_source_code)) {
                         openURL(context, "https://github.com/xDMPx/NormsCount")
                     }
-                    AboutButton(text = stringResource(R.string.about_author)) {
-                        openURL(context, "https://github.com/xDMPx")
-                    }
                     AboutButton(text = stringResource(R.string.about_license)) {
                         openURL(context, "https://github.com/xDMPx/NormsCount/blob/main/LICENSE")
+                    }
+                    AboutButton(text = stringResource(R.string.about_author)) {
+                        openURL(context, "https://github.com/xDMPx")
                     }
                 }
             }
@@ -96,10 +105,16 @@ object About {
     private fun AppInfo(modifier: Modifier = Modifier) {
         val context = LocalContext.current
         Box(modifier = modifier.fillMaxWidth()) {
-            Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     AppIcon()
-                    Text(text = context.getString(context.applicationInfo.labelRes), fontWeight = FontWeight.W500)
+                    Text(
+                        text = context.getString(context.applicationInfo.labelRes),
+                        fontWeight = FontWeight.W500
+                    )
                 }
                 AppDescription()
             }
