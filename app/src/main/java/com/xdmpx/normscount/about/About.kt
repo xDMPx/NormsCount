e9af201e71f52799612d99b32198082f03c3285d
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-03-16 13:41:18 +0100

    Update about screen to display app icon, description, and name

--- app/src/main/java/com/xdmpx/normscount/about/About.kt
+++ app/src/main/java/com/xdmpx/normscount/about/About.kt
@@ -2,14 +2,18 @@ package com.xdmpx.normscount.about
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -23,11 +27,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat.startActivity
+import androidx.core.graphics.drawable.toBitmap
 import com.xdmpx.normscount.BuildConfig
 import com.xdmpx.normscount.R
 
@@ -48,6 +55,7 @@ object About {
                     .fillMaxSize()
             ) {
                 Column {
+                    AppInfo()
                     AboutButton(text = "${stringResource(R.string.about_version)} v${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})") {}
                     AboutButton(text = stringResource(R.string.about_source_code)) {
                         openURL(context, "https://github.com/xDMPx/NormsCount")
@@ -82,6 +90,42 @@ object About {
                 overflow = TextOverflow.Ellipsis
             )
         }, actions = {})
+    }
+
+    @Composable
+    private fun AppInfo(modifier: Modifier = Modifier) {
+        val context = LocalContext.current
+        Box(modifier = modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    AppIcon()
+                    Text(text = context.getString(context.applicationInfo.labelRes), fontWeight = FontWeight.W500)
+                }
+                AppDescription()
+            }
+        }
+    }
+
+    @Composable
+    private fun AppIcon(modifier: Modifier = Modifier) {
+        val drawable =
+            LocalContext.current.packageManager.getApplicationIcon(BuildConfig.APPLICATION_ID)
+        Box(contentAlignment = Alignment.Center, modifier = modifier) {
+            Image(
+                drawable.toBitmap(config = Bitmap.Config.ARGB_8888).asImageBitmap(),
+                contentDescription = "App Icon",
+                modifier = Modifier
+                    .size(75.dp)
+                    .padding(8.dp)
+            )
+        }
+    }
+
+    @Composable
+    private fun AppDescription(modifier: Modifier = Modifier) {
+        Box(modifier = modifier) {
+            Text(text = stringResource(id = R.string.about_app_description))
+        }
     }
 
     @Composable
