c71dc6d662c5319c06a3945c7718d7d9179770f9
xDMPx <57721731+xDMPx@users.noreply.github.com>
2026-02-11 17:41:58 +0100

    Bump compileSdk and targetSdk to 36. Resolves AAR metadata issues when upgrading dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.xdmpx.normscount"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.xdmpx.normscount"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
